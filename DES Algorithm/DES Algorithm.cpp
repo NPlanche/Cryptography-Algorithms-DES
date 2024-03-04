5aa6e2979eab4b6948f583940822a0c301e48b5a
NPlanche <89564236+NPlanche@users.noreply.github.com>
2024-03-04 06:29:48 -0500

    s-boxes

--- DES Algorithm/DES Algorithm.cpp
+++ DES Algorithm/DES Algorithm.cpp
@@ -141,16 +141,58 @@ string ptToBinary(string pt) {
 
 string DES() {
 
+    //Sunstitution Boxes
+    int s_boxes[8][4][16] = {
+        {
+            14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+            0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+            4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+            15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13
+        },
+        {
+            15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+            3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+            0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+            13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9
+        },
+        {
+            10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+            13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+            13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12 ,5, 10, 14, 7,
+            1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12
+        },
+        {
+            7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+            13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+            10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+            3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7 ,2 ,14
+
+        },
+        {
+            2 12 4 1 7 10 11 6 8 5 3 15 13 0 14 9
+            14 11 2 12 4 7 13 1 5 0 15 10 3 9 8 6
+            4 2 1 11 10 13 7 8 15 9 12 5 6 3 0 14
+            11 8 12 7 1 14 2 13 6 15 0 9 10 4 5 3
+        },
+        {},
+        {},
+        {},
+    };
+
+
     string ip = initial_permutation();
 
     //divide into two halves
     string left = ip.substr(0, 32);
     string right = ip.substr(32, 32);
 
-    //Encryption
-    string right_expansion = expansion(right);
-    string xor_exp = Xor(keys[i], right_expansion);
+    //Encryption 16 rounds 
+
+    for (int i = 0; i < 16; i++) {
+        string right_expansion = expansion(right);
+        string xor_exp = Xor(keys[i], right_expansion);
     
+    }
 
     
 }
@@ -200,16 +242,15 @@ string expansion(string right) {
     return right_expansion;
 }
 string Xor (string key_round,string exp){
-    {
-        string r = "";
-        for (int i = 0; i < exp.size(); i++) {
-            if (key_round[i] != exp[i]) {
-                r += "1";
-            }
-            else {
-                r += "0";
-            }
+    string r = "";
+    for (int i = 0; i < exp.size(); i++) {
+        if (key_round[i] != exp[i]) {
+            r += "1";
         }
+        else {
+            r += "0";
+        }
+    }
 
-        return r;
+    return r;
     }
